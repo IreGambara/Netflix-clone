87333efc4e2da4435b3a8b0a00b7cbb56412bf15
IreGambaraù <irene.gambara@gmail.com>
2022-03-14 16:42:47 +0100

    Versione Git netflix-clone

---
+++ src/requests.js
@@ -0,0 +1,15 @@
+
+const API_KEY = "6f4c5f6757d2ec507564c7674ca39a66";
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`, //il problema erano le virgolette!
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumantaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+  }
+
+export default requests;
